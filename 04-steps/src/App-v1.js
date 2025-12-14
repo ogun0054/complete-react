facb6f2da57e98c56d0e107dc1bc3ee4521bb211
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2025-12-13 21:30:52 -0500

    reuseable button for the step app

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -51,35 +51,21 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
-              text="Previous"
-              emoji="👈"
-            />
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
-              text="Next"
-              emoji="👉"
-            />
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, text, emoji }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      <span>{emoji}</span>
-      {text}
-    </button>
   );
 }
