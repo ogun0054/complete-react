aaa2a9b256d888ea6a85af1877fefa363ca9c324
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2025-12-19 22:57:27 -0500

    the static UI for the friend list is completed

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -18,3 +18,50 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.id} />
+      <h3> {friend.name} </h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          Your friend {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and friend {friend.name} are even!</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
